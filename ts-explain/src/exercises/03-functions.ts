e772b1dfac79699b665b1efde03d143d1381ddca
Jabi <michal.jabi@gmail.com>
2026-04-30 09:47:05 +0200

    task(03): make it work.

--- ts-explain/src/exercises/03-functions.ts
+++ ts-explain/src/exercises/03-functions.ts
@@ -1,5 +1,5 @@
 /**
- * Zadanie 3:
+ * ⚠️⚠️⚠️ Zadanie NA START 3:
  *
  * Napisz logikę do funkcji userOnboarding()
  *
@@ -38,11 +38,30 @@
 const JOB_TITLE = "Senior Specialist";
 const SOCIAL_PACKET = "Grand premium Sport Card";
 
-// @ts-ignore
-function userOnboarding(name, yearOfExperience, jobTitle, socialPacket) {
-  console.log("awaiting logic...");
+
+function userOnboarding(name: string, yearOfExperience: null | number, jobTitle = 'Senior Specialist', socialPacket = 'Grand premium Sport Card') {
+//   console.log(name);
+//   console.log(yearOfExperience);
+//   console.log(jobTitle);
+//   console.log(socialPacket);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_NOT
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing
+  console.log(`
+     Witaj ${ name },
+     mając ${ yearOfExperience ?? 5 } lat doświadczenia.
+     Dostałeś stanowisko: ${ jobTitle }
+     Przysługuje Ci: ${ socialPacket }
+`);
 }
 
-// @ts-ignore
-userOnboarding("Marek");
+
+userOnboarding("Marek", null);
+// tak naprawdę dzieje się tak:
+//userOnboarding("Marek", undefined, undefined, undefined);
+
+
 userOnboarding("Jacek", 20, "Manager", "Medical pack");
