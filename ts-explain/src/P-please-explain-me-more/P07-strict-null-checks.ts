18efb245ecab52beb9432b2492ec61f7061d0cec
Jabi <michal.jabi@gmail.com>
2026-04-30 11:44:27 +0200

    explain: srtictNullChecks as a `hack` to deal with `| null` and `| undefined`.
    moral: use only in JS to TS migrations!

---
+++ ts-explain/src/P-please-explain-me-more/P07-strict-null-checks.ts
@@ -0,0 +1,12 @@
+
+// ✅ srtictNullChecks : true 
+let name: string = '';
+// ❌ srtictNullChecks : true 
+let nameWithoutValue: string;
+// nie da się użyć bo nie zadeklarowaliśmy faktycznej wartości typu string:
+// console.log(nameWithoutValue)
+
+// ✅ srtictNullChecks: false
+let name2: string; // wtedy tak naprawdę name2 to `undefined` ⚠️ ale TS pozwoli nam to użyć, bo wyłączamy flagę strictNullChecks
+
+// console.log(name2);
