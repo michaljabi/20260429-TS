966600fb56894d7ad222118a49b2065cd016b404
Jabi <michal.jabi@gmail.com>
2026-04-30 12:57:58 +0200

    refactor(06): make it right - better typing. Tuple + interface / type + unions

--- ts-explain/src/exercises/06-shape-of-data.ts
+++ ts-explain/src/exercises/06-shape-of-data.ts
@@ -11,25 +11,51 @@
  * - utwórz odpowiednie obiekty i przekaż do `giveFullName` tak aby potwierdzić poprawność działania kodu
  * */
 
-
 // Wniosek Array to typ tzw. generyczny
 // to znaczy, że w takcie użycia wymaga dodefiniowania (doprecyzowania).
 // Array<string> === string[]
 // Array<string | number> === (string | number)[]
 // działa wnioskowanie:
-const numbersOrStrings = ['', 0];
+const numbersOrStrings = ["", 0];
 const numbersOrStrings2: (string | number)[] = [];
 const numbersOrStrings3: Array<string | number> = [];
-const strings = [''];
+const strings = [""];
 
-function joiner(strTuple: string[]): string {
+function joiner(strTuple: [string, string]): string {
   return strTuple[0] + " " + strTuple[1];
 }
 
-function giveFullName(myUser: {name: string, lastName: string, sex: string, vipStatus?: boolean}): string {
+const realTuple: [string] = ["?"];
+
+// tak możemy tylko z type (nie da się z interface - interface jest ZAWSZE obiektowy!)
+type Money = number | string;
+
+type Person = {
+  name: string;
+  lastName: string;
+  sex: "female" | "male" | "not-set";
+  vipStatus?: boolean;
+};
+// to w takim wypadku type - czy interface?
+
+// rozszerzenie instiejącego typu Person o dodatkowe opcjonalne pole age - przez
+interface Person2 extends Person {
+  // name: string;
+  // lastName: string;
+  // sex: "female" | "male" | "not-set";
+  // vipStatus?: boolean;
+  age?: number;
+}
+
+// rozszerzanie typów o dodatkowe pola... dodajemy wymagane `age`
+type AgedPerson = Person & { age: number };
+
+const myPerson: AgedPerson = { name: '', lastName: '', sex: 'female', age: 19 };
+
+function giveFullName(myUser: Person2): string {
   const fullName = myUser.name + " " + myUser.lastName;
-  let prefix = '';
-  let suffix = '';
+  let prefix = "";
+  let suffix = "";
   if (myUser.sex === "female") {
     prefix = "Pani";
   } else if (myUser.sex === "male") {
@@ -43,6 +69,13 @@ function giveFullName(myUser: {name: string, lastName: string, sex: string, vipS
 }
 
 // rozwiązane, ale celowa literówka
-console.log(giveFullName({name: 'Michał', lastName: 'Kowalsky', sex: 'malez', vipStatus: true}));
+console.log(
+  giveFullName({
+    name: "Michał",
+    lastName: "Kowalsky",
+    sex: "male",
+    vipStatus: true,
+  }),
+);
 
-console.log(joiner(['pani', 'Kasia']))
+console.log(joiner(["pani", "Kasia"]));
