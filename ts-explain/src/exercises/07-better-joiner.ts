24e4c18a6f0ebec64df714f97f21a3d8859d398c
Jabi <michal.jabi@gmail.com>
2026-04-30 14:19:13 +0200

    task(07): solved + explain function returning a fuction.

--- ts-explain/src/exercises/07-better-joiner.ts
+++ ts-explain/src/exercises/07-better-joiner.ts
@@ -10,8 +10,17 @@
  *
  * */
 
-function joiner(joinString: any): any {
-  return (...strings: any): any => {
+const array: string[] = [''];
+
+array.push('2', '3', '4', '5')
+array.unshift()
+
+type JoinerFn = (...strsToJoin: string[]) => string;
+
+function joiner(joinString: string): JoinerFn {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+  return (...strings: string[]): string => {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
     return strings.join(joinString);
   };
 }
@@ -23,3 +32,28 @@ const snakeJoiner = joiner("_");
 console.log(hyphenJoiner("this", "is", "sample"));
 console.log(spaceJoiner("this", "is", "other", "sample"));
 console.log(snakeJoiner("how", "does", "snake", "case", "looks", "like"));
+
+const elements = ['a', 'b', 'c', 'd', 'e'];
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+console.log(hyphenJoiner(...elements))
+
+
+// Kiedy w JS potrzebujemy żeby funkcja zwracała funkcje:
+
+const numbers = [1, 2, 3];
+const doubleNumbers = numbers.map(e => e * 2);
+
+console.log(numbers)
+console.log(doubleNumbers)
+console.log(numbers)
+
+// Wykorzystanie funkcji, która zraca funkcję do mapowania
+function multiplyBy(multiplier: number) {
+   return (value:number ) => value * multiplier
+}
+
+// refactoring (one liner):
+const multiplyBy2 = (multiplier: number) => (value:number ) => value * multiplier
+
+const multiplyNumbersBy = numbers.map(multiplyBy(4));
+console.log(multiplyNumbersBy)
